0cb6c5f351935d7912353eebc54452a8f9d35a33
Jesus Guerrero <jesusguerrerogonzalez2@gmail.com>
2023-02-02 18:15:54 +0100

    Funcionando todo, solo falta swagger y demas (explicado en la clase REST)

--- src/main/java/es/iescabeza/aulasinformaticapsptr18/controller/MyRestController.java
+++ src/main/java/es/iescabeza/aulasinformaticapsptr18/controller/MyRestController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +19,12 @@ import java.util.Optional;
 @RequestMapping(value = "/reservas", produces = {"application/json"})
 public class MyRestController
 {
+
+    List<ReservaAula> listaReservasAulas = new ArrayList<>();
+
+    List<ReservaCarritoTablets> listaReservasCarritoTablets = new ArrayList<>();
+
+    List<ReservaCarritoPcs> listaReservasCarritoPcs = new ArrayList<>();
 
     @Autowired
     private AllReservasService allReservasService;
@@ -111,9 +118,13 @@ public class MyRestController
         reservaAula.setReservaAulaId(reservaAulaId);
         reservaAula.setIdProfesor(profesor);
 
-        this.iReservaAulaRepository.saveAndFlush(reservaAula);
+        listaReservasAulas.add(reservaAula);
 
-        return new ResponseEntity<String>("Reserva de Aula realizada correctamente", HttpStatus.OK);
+        sesion.setAttribute("reservaAula", listaReservasAulas);
+
+        //this.iReservaAulaRepository.saveAndFlush(reservaAula);
+
+        return new ResponseEntity<String>("Reserva de Aula guardada en sesion correctamente", HttpStatus.OK);
 
     }
 
@@ -172,9 +183,13 @@ public class MyRestController
         reservaCarritoTablets.setIdProfesor(profesor);
         reservaCarritoTablets.setUbicacionPrestamo(ubicacionPrestamoTablets);
 
-        this.iReservaCarritoTabletsRepository.saveAndFlush(reservaCarritoTablets);
+        //this.iReservaCarritoTabletsRepository.saveAndFlush(reservaCarritoTablets);
 
-        return new ResponseEntity<String>("Reserva de carro de Tablets realizada correctamente", HttpStatus.OK);
+        listaReservasCarritoTablets.add(reservaCarritoTablets);
+
+        sesion.setAttribute("reservaCarritoTablets", listaReservasCarritoTablets);
+
+        return new ResponseEntity<String>("Reserva de carro de Tablets guardada en sesion correctamente", HttpStatus.OK);
 
     }
 
@@ -231,9 +246,13 @@ public class MyRestController
         reservaCarritoPcs.setUbicacionPrestamo(ubicacionPrestamoPcs);
         reservaCarritoPcs.setReservaCarrtitoPcsId(reservaCarritoPcsId);
 
-        this.iReservaCarritoPcsRepository.saveAndFlush(reservaCarritoPcs);
+        //this.iReservaCarritoPcsRepository.saveAndFlush(reservaCarritoPcs);
 
-        return new ResponseEntity<String>("Reserva de carro de Pcs realizada correctamente", HttpStatus.OK);
+        listaReservasCarritoPcs.add(reservaCarritoPcs);
+
+        sesion.setAttribute("reservasCarritoPcs", listaReservasCarritoPcs);
+
+        return new ResponseEntity<String>("Reserva de carro de Pcs guardada en sesion correctamente", HttpStatus.OK);
     }
 
     /**
@@ -242,15 +261,182 @@ public class MyRestController
      * @return
      */
     @RequestMapping(method = RequestMethod.POST, value = "/confirmReservation")
-    public ResponseEntity<?> confirmarReserva(HttpSession sesion,
-                                              @RequestParam(value = "confirmacion", required = true) final String confirmacion)
+    public ResponseEntity<?> confirmarReserva(HttpSession sesion)
     {
 
-        return null;
+        if (sesion.getAttribute("reservaAula") != null)
+        {
+            this.iReservaAulaRepository.saveAllAndFlush(listaReservasAulas);
+        }
+
+        if (sesion.getAttribute("reservaCarritoTablets") != null)
+        {
+            this.iReservaCarritoTabletsRepository.saveAllAndFlush(listaReservasCarritoTablets);
+        }
+
+        if (sesion.getAttribute("reservasCarritoPcs") != null)
+        {
+            this.iReservaCarritoPcsRepository.saveAllAndFlush(listaReservasCarritoPcs);
+        }
+
+        sesion.invalidate();
+
+        return new ResponseEntity<String>("Reservas realizadas correctamente", HttpStatus.OK);
 
     }
 
     // hacer 3 endpoints mas con cancelar carrito pc, tablets y aulas
+
+    @RequestMapping(method = RequestMethod.POST, value = "/cancelReservation")
+    public ResponseEntity<?> cancelarSesiones(HttpSession sesion)
+    {
+
+        sesion.invalidate();
+
+        return new ResponseEntity<String>("Reservas almacenadas en sesion canceladas correctamente", HttpStatus.OK);
+
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/deleteReservaAula")
+    public ResponseEntity<?> deleteReservaAula(HttpSession sesion,
+                                        @RequestParam(value = "idProfesor", required = true) final Long idProfesor,
+                                        @RequestParam(value = "idAula", required = true) final Long idAula,
+                                        @RequestParam(value = "fechaReservaAula", required = true) final Long fechaReservaAula)
+    {
+
+        Date d = new Date(fechaReservaAula * 1000);
+
+        //Paso 1 instancias la reserva del aula ID
+        ReservaAulaId reservaAulaId = new ReservaAulaId();
+
+        reservaAulaId.setIdAula(idAula);
+        reservaAulaId.setIdProfesor(idProfesor);
+        reservaAulaId.setFecha(d);
+
+        //Paso 2 instanciar el profesor buscando por su id en su repositorio
+        Profesor profesor = new Profesor();
+
+        Optional<Profesor> optionalProfesor = iProfesorRepository.findById(idProfesor);
+
+        profesor.setId(optionalProfesor.get().getId());
+        profesor.setNombre(optionalProfesor.get().getNombre());
+        profesor.setApellidos(optionalProfesor.get().getApellidos());
+
+        //Paso 3 instanciar la reservaAula
+        ReservaAula reservaAula = new ReservaAula();
+
+        reservaAula.setReservaAulaId(reservaAulaId);
+        reservaAula.setIdProfesor(profesor);
+
+        iReservaAulaRepository.delete(reservaAula);
+
+        return new ResponseEntity<String>("Aula eliminada de la base de datos correctamente", HttpStatus.OK);
+
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/deleteReservaCarroTablets")
+    public ResponseEntity<?> deleteReservaCarroTablets(HttpSession sesion,
+                                           @RequestParam(value = "idProfesor", required = true) final Long idProfesor,
+                                           @RequestParam(value = "idCarritoTablets", required = true) final Long idCarritoTablets,
+                                           @RequestParam(value = "ubicacionPrestamoTablets", required = false) final String ubicacionPrestamoTablets,
+                                           @RequestParam(value = "fechaReservaAula", required = true) final Long fechaReservaAula)
+    {
+
+        Date d = new Date(fechaReservaAula * 1000);
+
+        //Paso 1 instancias la reserva del aula ID
+
+        ReservaCarritoTabletsId reservaCarritoTabletsId = new ReservaCarritoTabletsId();
+
+        reservaCarritoTabletsId.setIdCarritoTablets(idCarritoTablets);
+        reservaCarritoTabletsId.setIdProfesor(idProfesor);
+        reservaCarritoTabletsId.setFecha(d);
+
+        //Paso 2 instanciar el profesor buscando por su id en su repositorio
+        Profesor profesor = new Profesor();
+
+        Optional<Profesor> optionalProfesor = iProfesorRepository.findById(idProfesor);
+
+        profesor.setId(optionalProfesor.get().getId());
+        profesor.setNombre(optionalProfesor.get().getNombre());
+        profesor.setApellidos(optionalProfesor.get().getApellidos());
+
+        //Paso 3 instanciar los carritosTablest
+
+        CarritoTablets carritoTablets = new CarritoTablets();
+        Optional<CarritoTablets> optionalCarritoTablets = iCarritoTabletsRepository.findById(idCarritoTablets);
+
+        carritoTablets.setId(optionalCarritoTablets.get().getId());
+        carritoTablets.setNumeroTablets(optionalCarritoTablets.get().getNumeroTablets());
+        carritoTablets.setPlanta(optionalCarritoTablets.get().getPlanta());
+
+
+        //Paso 4 instanciar la reserva del carrito
+        ReservaCarritoTablets reservaCarritoTablets = new ReservaCarritoTablets();
+        reservaCarritoTablets.setReservaCarritoTabletsId(reservaCarritoTabletsId);
+        reservaCarritoTablets.setIdCarritoTablets(carritoTablets);
+        reservaCarritoTablets.setIdProfesor(profesor);
+        reservaCarritoTablets.setUbicacionPrestamo(ubicacionPrestamoTablets);
+
+        this.iReservaCarritoTabletsRepository.delete(reservaCarritoTablets);
+
+        return new ResponseEntity<String>("Reserva de carro eliminada de la base de datos correctamente", HttpStatus.OK);
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/deleteReservaCarroPcs")
+    public ResponseEntity<?> deleteReservaCarroPcs(HttpSession sesion,
+                                       @RequestParam(value = "idProfesor", required = true) final Long idProfesor,
+                                       @RequestParam(value = "idCarritoPcs", required = true) final Long idCarritoPcs,
+                                       @RequestParam(value = "ubicacionPrestamoPcs", required = false) final String ubicacionPrestamoPcs,
+                                       @RequestParam(value = "fechaReservaAula", required = true) final Long fechaReservaAula)
+    {
+
+        Date d = new Date(fechaReservaAula * 1000);
+
+        //Paso 1 instancias la reserva del aula ID
+
+        ReservaCarritoPcsId reservaCarritoPcsId = new ReservaCarritoPcsId();
+
+        reservaCarritoPcsId.setIdCarritoPcs(idCarritoPcs);
+        reservaCarritoPcsId.setIdProfesor(idProfesor);
+        reservaCarritoPcsId.setFecha(d);
+
+        //Paso 2 instanciar el profesor buscando por su id en su repositorio
+        Profesor profesor = new Profesor();
+
+        Optional<Profesor> optionalProfesor = iProfesorRepository.findById(idProfesor);
+
+        profesor.setId(optionalProfesor.get().getId());
+        profesor.setNombre(optionalProfesor.get().getNombre());
+        profesor.setApellidos(optionalProfesor.get().getApellidos());
+
+        //Paso 3 instanciar los carritosTablest
+
+        CarritoPc carritoPc = new CarritoPc();
+        Optional<CarritoPc> optionalCarritoPc = iCarritoPcRepository.findById(idCarritoPcs);
+
+        carritoPc.setId(optionalCarritoPc.get().getId());
+        carritoPc.setNumeroPcs(optionalCarritoPc.get().getNumeroPcs());
+        carritoPc.setPlanta(optionalCarritoPc.get().getPlanta());
+
+
+        //Paso 4 instanciar la reserva del carrito
+        ReservaCarritoPcs reservaCarritoPcs = new ReservaCarritoPcs();
+        reservaCarritoPcs.setIdCarritoPcs(carritoPc);
+        reservaCarritoPcs.setIdProfesor(profesor);
+        reservaCarritoPcs.setUbicacionPrestamo(ubicacionPrestamoPcs);
+        reservaCarritoPcs.setReservaCarrtitoPcsId(reservaCarritoPcsId);
+
+        this.iReservaCarritoPcsRepository.delete(reservaCarritoPcs);
+
+        return new ResponseEntity<String>("Reserva de carro de Pcs eliminada de la base de datos correctamente", HttpStatus.OK);
+    }
+
+    //implementa todos los metodos de delete en swagger que suspendes subnormal
+    //ademas de añadir en la base de datos por defecto unos carritos y aulas
+    //y acuerdate de hacer un txt con las instrucciones que debe seguir el profesor
+    //Y documenta algo que luego se te olvida
 
 }
 
