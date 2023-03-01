67096e01293c576a66216ca96cfff81f5f41f92e
Jesus Guerrero <jesusguerrerogonzalez2@gmail.com>
2023-03-01 16:40:28 +0100

    implementando seguridad funcionando completa

--- src/main/java/es/iescabeza/aulasinformaticapsptr18/security/SpringSecurityConfig.java
+++ src/main/java/es/iescabeza/aulasinformaticapsptr18/security/SpringSecurityConfig.java
@@ -8,21 +8,32 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Clase de configuracion de seguridad
+ * Encargada de autorizar a los usuarios creados a acceder a los endpoints
+ */
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig
 {
 
+    /**
+     * Cadena de seguridad del filtro
+     * @param http http
+     * @return devuelve la construccion del http
+     * @throws Exception exception
+     */
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception
     {
 
-        http.authorizeHttpRequests()
+        http.csrf().disable().authorizeHttpRequests()
 
                 .requestMatchers("/reservas/allReservations",
                         "/reservas/selectAula", "/reservas/selectTablets", "/reservas/selectPcs",
                         "/reservas/confirmReservation", "/reservas/cancelReservation", "/reservas/deleteReservaAula",
                         "/reservas/deleteReservaCarroTablets", "/reservas/deleteReservaCarroPcs").hasAnyRole("USER", "ADMIN")
+                .anyRequest().authenticated()
                 .and().formLogin()
                 .and().httpBasic();
 
@@ -30,6 +41,10 @@ public class SpringSecurityConfig
 
     }
 
+    /**
+     * Usuarios almacenados en memoria
+     * @return usuarios en memoria
+     */
     @Bean
     public InMemoryUserDetailsManager userDetailsManager()
     {
